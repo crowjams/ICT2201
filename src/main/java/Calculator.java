d29773674af7ab865a43d3d444f311933ca92399
Jonathan <crowjams@gmail.com>
2017-11-10 10:47:10 +0800

    changed src cdoe

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,6 @@
 public class Calculator {
   public int evaluate(String expression) {
-    int sum = 12;
+    int sum = 121234234231;
     for (String summand: expression.split("\\+"))
       sum += Integer.valueOf(summand);
     return sum;
